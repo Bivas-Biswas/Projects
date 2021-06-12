fc2b1b974b5829a7cce3f4c0af62d9cf4367aa6d
BivasBiswas <bivasbiswas1999@gmail.com>
2021-06-12 21:24:04 +0530

    explain the js code line by line

--- JavaScript + CSS Range Slider/slider.js
+++ JavaScript + CSS Range Slider/slider.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector('#myrange')
 const output = document.querySelector('#value')
 
+// udate the value according to the slider value
 output.innerHTML = slider.value
 
 // check slider value is greater than 0
@@ -11,10 +12,12 @@ if (slider.value > 0 || slider.value < 0 ){
     console.log(color)
 }
 
+// to update output value, according the move of the slider value
 slider.oninput = function (){
     output.innerHTML = this.value
 }
 
+// update the slider background value, according to the slider thumb position
 slider.addEventListener('input', function (){
     const x = slider.value
     const color = 'linear-gradient(90deg, rgb(117, 252, 117)' + x/2 + '%, rgb(214,214,214)' + x/2 + '%)'
